1ff6fec258b3e03c688b5c933ec0dfa266c1512a
Warda Bounab <wardabounab@MacBook-Pro-de-Warda.local>
2024-10-11 18:43:45 +0100

    feat(checkpoint typescript

---
+++ app.ts
@@ -0,0 +1,4 @@
+import { Car } from "./classe";
+
+const myCar = new Car("Toyota", "Corolla", 2021);
+myCar.start(); // Affiche: "Car engine started"
---
+++ js/app.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var classe_1 = require("./classe");
+var myCar = new classe_1.Car("Toyota", "Corolla", 2021);
+myCar.start(); // Affiche: "Car engine started"
